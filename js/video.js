1b8e043dff817c353c97509474865047e6182653
Zoe Corser <zcorser@umich.edu>
2024-11-22 22:48:49 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -2,10 +2,27 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
+	video = document.getElementById("player1");
+	// Turn off autoplay
+	video.autoplay = false;
+	// Turn off looping
+	video.loop = false;
+
+    // Update volume information
+    var volumeDisplay = document.getElementById("volume");
+    video.volume = 0.5; // Default volume to 50%
+    volumeDisplay.innerHTML = video.volume * 100 + '%';
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	updateVolumeInfo();
+	});
 
+    document.getElementById("pause").addEventListener("click", pauseVideo);
+	function pauseVideo() {
+		console.log("Pause Video");
+		video.pause();
+	}
